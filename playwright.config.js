84f6a70a7af1bb6c2efecbef40cd8c5075acb34f
Abubakarsiddiq9 <abubakarsiddiqmohiuddin@gmail.com>
2026-07-08 18:34:57 +0530

    dbg pckjson

--- playwright.config.js
+++ playwright.config.js
@@ -73,9 +73,11 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: "npm run dev",
     url: "http://127.0.0.1:8787",
     reuseExistingServer: !process.env.CI,
-  },
+    stdout: "pipe",
+    stderr: "pipe",
+  }
 });
 
